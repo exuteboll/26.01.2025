980f36fb78ae3d8d2e870c1ff4eb8d3722b2ed42
exuteboll <exuteboll4@gmail.com>
2025-01-26 17:38:58 +0200

    Add files via upload

---
+++ ConsoleApp7/Books.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    // 1) Создать класс Books («Книги»), содержащий следующие члены:
+
+    //1. поля: название, автор, год выпуска, объем в листах.
+
+    //2. метод Info, позволяющий выводить на экран всю информацию об объекте.
+
+    //2) Добавить UML-диаграмму классов Book и Program.
+
+
+    internal class Books
+    {
+        public string Name = "Трансферинг себя";
+        public string Author = "Вадим Зеланд";
+        public int Data = 2024;
+        public int Page = 387;
+        public void Info()
+        {
+            Console.WriteLine($"Название книги: {Name}\nАвтор книги: {Author}\nГод выпуска: {Data}\nОбъем страниц: {Page}");
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1) Создать класс Books («Книги»), содержащий следующие члены:
+
+            //1. поля: название, автор, год выпуска, объем в листах.
+
+            //2. метод Info, позволяющий выводить на экран всю информацию об объекте.
+
+            //2) Добавить UML-диаграмму классов Book и Program.
+
+
+            Books books = new Books();
+            string name = books.Name;
+            string author = books.Author;
+            int data = books.Data;
+            int page = books.Page;
+            books.Info();
+            Console.ReadKey();
+        }
+    }
+}
